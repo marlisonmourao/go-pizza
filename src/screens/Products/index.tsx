943707aa774eebcdd86b1051351861f39a4d633a
marlisonmourao <bentesmourao@gmail.com>
2023-04-25 12:55:28 -0400

    load image

--- src/screens/Products/index.tsx
+++ src/screens/Products/index.tsx
@@ -1,10 +1,37 @@
+import { useState } from 'react'
 import { Platform, TouchableOpacity } from 'react-native'
-import { Container, DeleteLabel, Header, Title } from './styles'
+import * as ImagePicker from 'expo-image-picker'
+
+import {
+  Container,
+  DeleteLabel,
+  Header,
+  PickImageButton,
+  Title,
+  Upload,
+} from './styles'
 
 import { ButtonBack } from '@components/ButtonBack'
 import { Photo } from '@components/Photo'
 
 export function Products() {
+  const [image, setImage] = useState('')
+
+  async function handlePickerImage() {
+    const { status } = await ImagePicker.requestCameraPermissionsAsync()
+
+    if (status === 'granted') {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [4, 4],
+      })
+
+      if (!result.canceled) {
+        setImage(result.assets[0].uri)
+      }
+    }
+  }
+
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
       <Header>
@@ -15,7 +42,15 @@ export function Products() {
         </TouchableOpacity>
       </Header>
 
-      <Photo uri="" />
+      <Upload>
+        <Photo uri={image} />
+
+        <PickImageButton
+          title="Carregar"
+          type="secondary"
+          onPress={handlePickerImage}
+        />
+      </Upload>
     </Container>
   )
 }
